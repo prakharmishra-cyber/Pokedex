32436b2f76980f0de2282a05fe3a49318d9e09ac
Prakhar Mishra <mprakhar35@gmail.com>
2021-12-15 08:05:52 +0000

    2nd commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [user, setUsers]=useState([{
+    name:'Prakhar Mishra',
+    age:15
+  },])
+  const [new_name, setNew_Name]=useState('');
+
+  const handleSubmit = () =>{
+    setUsers([...user, {
+      name:new_name,
+      age:15
+    }]);
+    setNew_Name('');
+  }
+
+
   return (
     <div className="App">
-      hello World
+      {user.map(e=>{
+        return (
+        <div>His name is {e.name} and his age is {e.age}</div>
+        )
+      })}
+      <input placeholder='Enter the new name' value={new_name} onChange={e=>setNew_Name(e.target.value)}/>
+      <button onClick={handleSubmit}>Add +</button>
     </div>
+    
   );
 }
 
